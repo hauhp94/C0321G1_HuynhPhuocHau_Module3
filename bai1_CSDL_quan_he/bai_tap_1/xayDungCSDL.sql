d2f42b6254b23f04b60d847d902a694fa2c739f8
Hau HP <hauhpdn2015@gmail.com>
2021-06-10 08:31:23 +0700

    bt

--- bai1_CSDL_quan_he/bai_tap_1/xayDungCSDL.sql
+++ bai1_CSDL_quan_he/bai_tap_1/xayDungCSDL.sql
@@ -1,18 +1,18 @@
 CREATE DATABASE `student-management`;
 CREATE TABLE `student-management`.`student`(
 `id` INT NOT NULL,
-`name` VARCHAR(50) NULL,
-`age` INT NULL,
-`country` VARCHAR(50) NULL,
+`name` VARCHAR(50),
+`age` INT ,
+`country` VARCHAR(50) ,
 PRIMARY KEY (`id`));
 CREATE TABLE `student-management`.`Class`(
 `id` INT NOT NULL,
-`name` VARCHAR(50) NULL,
+`name` VARCHAR(50) ,
 PRIMARY KEY (`id`));
 CREATE TABLE `student-management`.`Teacher`(
 `id` INT NOT NULL,
-`name` VARCHAR(50) NULL,
-`age` INT NULL,
-`country` VARCHAR(50) NULL,
+`name` VARCHAR(50) ,
+`age` INT ,
+`country` VARCHAR(50) ,
 PRIMARY KEY (`id`));
 
