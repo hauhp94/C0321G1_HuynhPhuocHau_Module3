0a8a9fd4177bc5c5c7658b29f96ba62ad174228d
Hau HP <hauhpdn2015@gmail.com>
2021-06-16 22:48:20 +0700

    bt

--- casetudy_database/furama.sql
+++ casetudy_database/furama.sql
@@ -123,15 +123,17 @@ values('Đặng Văn Lâm', 1,2,1,'1990-03-02', 123123123, 15000.0, 0905123345,'
  values ('Diamond'),('Platinium'),('Gold'),('Silver'),('Member');
  insert into khach_hang(id_loai_khach ,ho_ten_khach,ngay_sinh ,so_cmnd,sdt ,email,dia_chi)
  values(2,'Nguyễn Quang Hải','1990-02-03',123234123,0905123123,'hai@gmail.com','Hà Nội'),
- (3,'Bùi Tấn Trường','1990-02-03',123234123,0905123123,'truong@gmail.com','Đà Nẵng'),
- (1,'Nguyễn Hoàng Đức','1995-02-03',155234123,0905123123,'duc@gmail.com','Huế'),
- (2,'Nguyễn Tiến Linh','1940-02-03',123234123,0905123123,'hai@gmail.com','Đà Nẵng'),
- (3,'Nguyễn Thành Chung','1990-02-03',123234123,0905123123,'truong@gmail.com','Quảng Trị'),
- (1,'Lê Văn Xuân','1995-02-03',155234123,0905123123,'xuan@gmail.com','Đà Nẵng'),
- (1,'Lê Văn Vip','1995-02-03',155234123,0905123123,'xuan@gmail.com','Quảng Nam'),
- (1,'Nguyễn Văn Sang','1995-02-03',155234123,0905123123,'xuan@gmail.com','Đà Lạt'),
- (1,'Võ Huy Toàn','2004-02-03',155234123,0905123123,'xuan@gmail.com','Hải Phòng'),
- (2,'Võ Huy Toàn','2005-02-03',123234123,0905843123,'toan2@gmail.com','Đà Nẵng');
+ (3,'Bùi Tấn Trường','1990-02-03',123234123,0925123376,'truong@gmail.com','Đà Nẵng'),
+ (1,'Nguyễn Hoàng Đức','1995-02-03',155234123,0777123345,'duc@gmail.com','Vinh'),
+ (2,'Nguyễn Tiến Linh','1940-02-03',123234123,0931948372,'hai@gmail.com','Đà Nẵng'),
+ (3,'Nguyễn Thành Chung','1990-02-03',123234123,0888111222,'truong@gmail.com','Quảng Ngãi'),
+ (1,'Lê Văn Xuân','1995-02-03',155234123,0123333222,'xuan@gmail.com','Đà Nẵng'),
+ (1,'Lê Văn Vip','1995-02-03',155234123,0993123234,'xuan@gmail.com','Quảng Ngãi'),
+ (1,'Nguyễn Văn Sang','1995-02-03',155234123,0989231453,'xuan@gmail.com','Vinh'),
+ (1,'Võ Huy Toàn','2004-02-03',155234123,0540234121,'xuan@gmail.com','Hải Phòng'),
+ (2,'Võ Huy Toàn','2005-02-03',123234123,0444222111,'toan2@gmail.com','Vinh'),
+  (5,'Võ Văn Dũng','2005-02-03',123234123,0543123333,'toan2@gmail.com','Trà Vinh'),
+   (4,'Bùi Huy Toàn','2005-02-03',123234123,0909202031,'toan2@gmail.com','Quảng Ngãi');
  insert into dich_vu(ten_dich_vu ,dien_tich,so_tang,so_nguoi_toi_da,chi_phi_thue,id_kieu_thue ,id_loai_dich_vu,trang_thai)
  values( 'villa mua dong', 90, 5,10,2000000,1,1,'trong'),
  ( 'villa mua he', 60, 5,10,2000000,1,1,'trong'),
@@ -141,11 +143,11 @@ values('Đặng Văn Lâm', 1,2,1,'1990-03-02', 123123123, 15000.0, 0905123345,'
  ( 'room mua dong', 56, 5,11,900000,3,3,'da co khach dat'),
  ('room mua dong', 78, 5,14,1000000,2,3,'trong');
  insert into dich_vu_di_kem(ten_dich_vu_di_kem,gia ,don_vi,trang_thai_kha_dung)
- values( 'massage', 200000, 2, 'trong'),
+ values( 'massage', 250000, 1, 'trong'),
  ( 'karaoke', 300000, 1, 'trong'),
- ( 'thức ăn', 100000, 3, 'trong'),
- ( 'nước uống', 50000,2 , 'trong'),
- ( 'thuê xe ', 600000, 2, 'trong');
+ ( 'thức ăn', 100000, 1, 'trong'),
+ ( 'nước uống', 50000,1 , 'trong'),
+ ( 'thuê xe ', 600000, 1, 'trong');
  insert into hop_dong(id_nhan_vien,id_khach_hang,id_dich_vu,ngay_lap_hop_dong ,ngay_ket_thuc ,tien_dat_coc,tong_tien )
  values(1,1,1,'2020-01-02','2021-03-29',5900000,null),
  (2,2,3,'2020-01-19','2021-03-15',2900000,null),
@@ -162,7 +164,7 @@ values('Đặng Văn Lâm', 1,2,1,'1990-03-02', 123123123, 15000.0, 0905123345,'
  (4,3,3,'2019-01-21','2016-03-01',12000000,null),
  (4,3,5,'2018-02-20','2016-03-01',4500000,null);
  insert into hop_dong_chi_tiet(id_hop_dong ,id_dich_vu_di_kem ,so_luong )
- values(2,1,1),(1,2,3),(1,2,3);
+ values(2,1,1),(1,2,3),(1,2,3),(1,2,2),(3,2,3),(4,2,5),(5,1,3),(6,4,3),(7,4,3),(8,2,3),(8,3,3),(9,5,1),(10,5,2),(11,2,1),(12,1,1),(13,2,3),(14,3,3);
  select * from hop_Dong_chi_tiet;
  -- Task 2 Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 ký tự.
 select* from nhan_vien 
@@ -184,11 +186,8 @@ order by so_lan_dat;
 /* Task 5 Hiển thị IDKhachHang, HoTen, TenLoaiKhach, IDHopDong, TenDichVu, NgayLamHopDong, NgayKetThuc, TongTien 
 (Với TongTien được tính theo công thức như sau: ChiPhiThue + SoLuong*Gia, với SoLuong và Giá là từ bảng DichVuDiKem)
  cho tất cả các Khách hàng đã từng đặt phỏng. (Những Khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).*/
- select * from hop_dong_chi_tiet;
- select * from  dich_vu_di_kem;
- select * from dich_vu;
- select * from hop_dong;
-select k.id_khach_hang, k.ho_ten_khach, l.ten_loai_khach, h.id_hop_dong,d.ten_dich_vu,h.ngay_lap_hop_dong, h.ngay_ket_thuc,d.chi_phi_thue, sum(d.chi_phi_thue+dk.don_vi*dk.gia) as tong_tien
+
+select k.id_khach_hang, k.ho_ten_khach, l.ten_loai_khach, h.id_hop_dong,d.ten_dich_vu,h.ngay_lap_hop_dong, h.ngay_ket_thuc,d.chi_phi_thue, d.chi_phi_thue+sum(dk.don_vi*dk.gia) as tong_tien
 from khach_hang k
 left join loai_khach l on k.id_loai_khach = l.id_loai_khach
 left join hop_dong h on h.id_khach_hang = k.id_khach_hang
@@ -197,7 +196,7 @@ left join hop_dong_chi_tiet hc on h.id_hop_dong = hc.id_hop_dong
 left join dich_vu_di_kem dk on hc.id_dich_vu_di_kem = dk.id_dich_vu_di_kem
 group by h.id_hop_dong
 union
-select k.id_khach_hang, k.ho_ten_khach, l.ten_loai_khach, h.id_hop_dong,d.ten_dich_vu,h.ngay_lap_hop_dong, h.ngay_ket_thuc,d.chi_phi_thue, sum(d.chi_phi_thue+dk.don_vi*dk.gia) as tong_tien
+select k.id_khach_hang, k.ho_ten_khach, l.ten_loai_khach, h.id_hop_dong,d.ten_dich_vu,h.ngay_lap_hop_dong, h.ngay_ket_thuc,d.chi_phi_thue, d.chi_phi_thue+sum(dk.don_vi*dk.gia) as tong_tien
 from khach_hang k
 right join loai_khach l on k.id_loai_khach = l.id_loai_khach
 right join hop_dong h on h.id_khach_hang = k.id_khach_hang
@@ -205,30 +204,11 @@ right join dich_vu d on h.id_dich_vu = d.id_dich_vu
 right join hop_dong_chi_tiet hc on h.id_hop_dong = hc.id_hop_dong
 right join dich_vu_di_kem dk on hc.id_dich_vu_di_kem = dk.id_dich_vu_di_kem
 group by h.id_hop_dong;
-
+ select * from hop_dong_chi_tiet;
+ select * from  dich_vu_di_kem;
+ select * from dich_vu;
+ select * from hop_dong;
 select ngay_lap_hop_dong from hop_dong;
-;
-select k.id_khach_hang,k.ho_ten_khach,l.ten_loai_khach,h.id_hop_dong,d.ten_dich_vu,h.ngay_lap_hop_dong,h.ngay_ket_thuc,d.chi_phi_thue,c.so_luong,dk.gia, sum(d.chi_phi_thue+hc.so_luong*dk.gia) as tong_tien
-from khach_hang k
-left join loai_khach l on k.id_loai_khach = l.id_loai_khach
-left join hop_dong h on k.id_khach_hang = h.id_khach_hang
-left join dich_vu d on h.id_dich_vu = d.id_dich_vu
-inner join hop_dong_chi_tiet hc on h.id_hop_dong = c.id_hop_dong
-inner join dich_vu_di_kem dk on hc.id_dich_vu_di_kem = dk.id_dich_vu_di_kem
-group by (h.id_hop_dong)
-having h.id_hop_dong is not null
-union
-select k.id_khach_hang,k.ho_ten_khach,l.ten_loai_khach,h.id_hop_dong,d.ten_dich_vu,h.ngay_lap_hop_dong,h.ngay_ket_thuc,d.chi_phi_thue,null,null,sum(d.chi_phi_thue+hc.so_luong*dk.gia) as tong_tien
-from khach_hang k
-left join loai_khach l on k.id_loai_khach = l.id_loai_khach
-left join hop_dong h on k.id_khach_hang = h.id_khach_hang
-left join dich_vu d on h.id_dich_vu = d.id_dich_vu
-where h.id_hop_dong not in (select hop_dong_chi_tiet.id_hop_dong from hop_dong_chi_tiet)
-union
-select k.id_khach_hang,k.ho_ten_khach,l.ten_loai_khach,null,null,null,null,null,null,null,null
-from khach_hang k
-left join loai_khach l on k.id_loai_khach = l.id_loai_khach
-where k.id_khach_hang not in (select hop_dong.id_khach_hang from hop_dong);
 select* from khach_hang;
 /* Task 6 Hiển thị IDDichVu, TenDichVu, DienTich, ChiPhiThue, TenLoaiDichVu 
 của tất cả các loại Dịch vụ chưa từng được Khách hàng thực hiện đặt từ quý 1 của năm 2019 (Quý 1 là tháng 1, 2, 3).*/
@@ -273,10 +253,103 @@ from hop_dong h
 inner join hop_dong_chi_tiet c on h.id_hop_dong = c.id_hop_dong
 inner join dich_vu_di_kem dk on c.id_dich_vu_di_kem = dk.id_dich_vu_di_kem
 group by (h.id_hop_dong);
--- Task 11 Hiển thị thông tin các Dịch vụ đi kèm đã được sử dụng bởi những Khách hàng có TenLoaiKhachHang là “Diamond” và có địa chỉ là “Vinh” hoặc “Quảng Ngãi”.
-select* from dich_vu_di_kem;
+/* Task 11 Hiển thị thông tin các Dịch vụ đi kèm đã được sử dụng bởi những Khách hàng
+có TenLoaiKhachHang là “Diamond” và có địa chỉ là “Vinh” hoặc “Quảng Ngãi”.*/
+select dvdk.ten_dich_vu_di_kem, dvdk.gia,kh.id_khach_hang,kh.ho_ten_khach,kh.dia_chi,lk.ten_loai_khach
+from dich_vu_di_kem dvdk
+join hop_dong_chi_tiet hdct on dvdk.id_dich_vu_di_kem =hdct.id_dich_vu_di_kem
+join hop_dong hd on hdct.id_hop_dong= hd.id_hop_dong
+join khach_hang kh on hd.id_khach_hang=kh.id_khach_hang
+join loai_khach lk on kh.id_loai_khach=lk.id_loai_khach
+where lk.ten_loai_khach='Diamond' and (kh.dia_chi='Vinh' or kh.dia_chi = 'Quảng Ngãi')
+group by hd.id_khach_hang;
+select* from hop_Dong;
+/* Task 12	Hiển thị thông tin IDHopDong, TenNhanVien, TenKhachHang, SoDienThoaiKhachHang,
+ TenDichVu, SoLuongDichVuDikem (được tính dựa trên tổng Hợp đồng chi tiết),
+ TienDatCoc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2019
+ nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2019.*/
+ select hd.id_hop_dong,nv.ho_ten as ten_nhan_vien, kh.ho_ten_khach as ten_khach_hang,kh.sdt as sdt_khach_hang,dv.ten_dich_vu, hdct.so_luong as so_luong_dvdk,hd.tien_dat_coc
+ from hop_dong_chi_tiet hdct
+ join hop_dong hd on hdct.id_hop_dong = hd.id_hop_dong
+ join nhan_vien nv  on hd.id_nhan_vien = nv.id_nhan_vien
+ join khach_hang kh on hd.id_khach_hang = kh.id_khach_hang
+ join dich_vu dv on hd.id_dich_vu=dv.id_dich_vu
+ join dich_vu_di_kem dvdk on hdct.id_dich_vu_di_kem=dvdk.id_dich_vu_di_kem
+ group by hd.id_hop_dong
+ order by hd.id_hop_dong;
+ -- Task 13 Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng.
+ -- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+ select dvdk.ten_dich_vu_di_kem, max(hdct.so_luong), kh.ho_ten_khach
+ from khach_hang kh
+  join hop_dong hd on kh.id_khach_hang = hd.id_khach_hang
+  join hop_dong_chi_tiet hdct on hd.id_hop_dong=hdct.id_hop_dong
+  join dich_vu_di_kem dvdk on hdct.id_dich_vu_di_kem= dvdk.id_dich_vu_di_kem
+  group by dvdk.ten_dich_vu_di_kem;
+  
+/* Task 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
+ Thông tin hiển thị bao gồm IDHopDong, TenLoaiDichVu, TenDichVuDiKem, SoLanSuDung. */
+ 
+/* Task 15.	Hiển thi thông tin của tất cả nhân viên bao gồm IDNhanVien, HoTen, TrinhDo, TenBoPhan, SoDienThoai,
+ DiaChi mới chỉ lập được tối đa 3 hợp đồng từ năm 2018 đến 2019.*/
+ 
+/* Task 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2017 đến năm 2019.*/
 
+/* Task 17.	Cập nhật thông tin những khách hàng có TenLoaiKhachHang từ  Platinium lên Diamond,
+ chỉ cập nhật những khách hàng đã từng đặt phòng với tổng Tiền thanh toán trong năm 2019 là lớn hơn 10.000.000 VNĐ.*/
+ 
+/* Task 18.	Xóa những khách hàng có hợp đồng trước năm 2016 (chú ý ràngbuộc giữa các bảng). */
 
+/* Task 19.	Cập nhật giá cho các Dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2019 lên gấp đôi.*/
+
+/* Task 20.	Hiển thị thông tin của tất cả các Nhân viên và Khách hàng có trong hệ thống,
+ thông tin hiển thị bao gồm ID (IDNhanVien, IDKhachHang),
+ HoTen, Email, SoDienThoai, NgaySinh, DiaChi.*/
+ 
+/* Task 21.	Tạo khung nhìn có tên là V_NHANVIEN để lấy được thông tin của tất cả các nhân viên có địa chỉ là “Hải Châu” 
+và đã từng lập hợp đồng cho 1 hoặc nhiều Khách hàng bất kỳ  với ngày lập hợp đồng là “12/12/2019”*/
+
+/* Task 22.	Thông qua khung nhìn V_NHANVIEN thực hiện cập nhật địa chỉ thành “Liên Chiểu”
+ đối với tất cả các Nhân viên được nhìn thấy bởi khung nhìn này.*/
+ 
+/* Task 23.	Tạo Clustered Index có tên là IX_KHACHHANG trên bảng Khách hàng.
+ Giải thích lý do và thực hiện kiểm tra tính hiệu quả của việc sử dụng INDEX*/
+ 
+/* Task 24.	Tạo Non-Clustered Index có tên là IX_SoDT_DiaChi trên các cột SODIENTHOAI và DIACHI trên bảng KHACH HANG 
+và kiểm tra tính hiệu quả tìm kiếm sau khi tạo Index.*/
+
+/* Task 25.	Tạo Store procedure Sp_1 Dùng để xóa thông tin của một Khách hàng nào đó 
+với Id Khách hàng được truyền vào như là 1 tham số của Sp_1*/
+
+/* Task 26.	Tạo Store procedure Sp_2 Dùng để thêm mới vào bảng HopDong 
+với yêu cầu Sp_2 phải thực hiện kiểm tra tính hợp lệ của dữ liệu bổ sung,
+ với nguyên tắc không được trùng khóa chính và đảm bảo toàn vẹn tham chiếu đến các bảng liên quan.*/
+ 
+/* Task 27.	Tạo triggers có tên Tr_1 Xóa bản ghi trong bảng HopDong thì 
+hiển thị tổng số lượng bản ghi còn lại có trong bảng HopDong ra giao diện console của database*/
+
+/* Task 28.	Tạo triggers có tên Tr_2 Khi cập nhật Ngày kết thúc hợp đồng,
+ cần kiểm tra xem thời gian cập nhật có phù hợp hay không,
+ với quy tắc sau: Ngày kết thúc hợp đồng phải lớn hơn ngày làm hợp đồng ít nhất là 2 ngày.
+ Nếu dữ liệu hợp lệ thì cho phép cập nhật,
+ nếu dữ liệu không hợp lệ thì in ra thông báo 
+ “Ngày kết thúc hợp đồng phải lớn hơn ngày làm hợp đồng ít nhất là 2 ngày” trên console của database*/
+ 
+/* Task 29.	Tạo user function thực hiện yêu cầu sau:
+a.	Tạo user function func_1: Đếm các dịch vụ đã được sử dụng với Tổng tiền là > 2.000.000 VNĐ.
+b.	Tạo user function Func_2: Tính khoảng thời gian dài nhất tính từ lúc bắt đầu làm hợp đồng
+ đến lúc kết thúc hợp đồng mà Khách hàng đã thực hiện thuê dịch vụ (lưu ý chỉ xét các khoảng
+ thời gian dựa vào từng lần làm hợp đồng thuê dịch vụ, không xét trên toàn bộ các lần làm hợp đồng).
+ Mã của Khách hàng được truyền vào như là 1 tham số của function này.*/
+
+/* Task 30.	Tạo Stored procedure Sp_3 để tìm các dịch vụ được thuê bởi khách hàng với loại dịch vụ là “Room” 
+từ đầu năm 2015 đến hết năm 2019 để xóa thông tin của các dịch vụ đó (tức là xóa các bảng ghi trong bảng DichVu) 
+và xóa những HopDong sử dụng dịch vụ liên quan (tức là phải xóa những bản gi trong bảng HopDong) và những bản liên quan khác.*/
+
+  
+  
+
+ 
+ 
 
 
 
