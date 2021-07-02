c29f2e9e15a9c969313687db85353b8a17748eb8
Hau HP <hauhpdn2015@gmail.com>
2021-07-02 19:12:25 +0700

    Bt

---
+++ bai14_Dependency_Management_Packaging/bai_tap_14/demo/src/DemoPTB2.java
@@ -0,0 +1,8 @@
+public class DemoPTB2 {
+    public static void main(String[] args) {
+        GiaiPhuongTrinhBac2 giaiPhuongTrinhBac2 = new GiaiPhuongTrinhBac2();
+        System.out.println(giaiPhuongTrinhBac2.giaiPTBac2(-4,3,2));
+        System.out.println(giaiPhuongTrinhBac2.giaiPTBac2(0,4,0));
+
+    }
+}
